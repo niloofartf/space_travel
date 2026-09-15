2a99ad8c9fbfdcd682bd5af9a012e8dcb67c2edc
Niloofar Torabifard <33426204+niloofartf@users.noreply.github.com>
2026-09-15 17:14:54 -0400

    Added customer_input and more advanced performance stats
    added customer_input with my info and random trip selection to test the input. My database and sql editor didnt allow for capturing input, but that may be doable on another environment.
    added advanced performance and success rates states for final scoring and ranking

--- Astra_EI_AgentsRanking.sql
+++ Astra_EI_AgentsRanking.sql
@@ -4,7 +4,7 @@ select
 --ah.customername,
 --ah.communicationmethod,
 --ah.leadsource,
-ah.agentid,
+s.agentid,
 ta.firstname,
 ta.lastname,
 ta.email,
@@ -19,40 +19,80 @@ ta.averagecustomerservicerating,
 
 
 
-SUM(b.packagerevenue) as packagerevenue,
-SUM(b.destinationrevenue) as destinationrevenue,
-SUM(b.totalrevenue) as totalrevenue,
 
 
 
-SUM(DISTINCT ah.AssignmentID) AS total_assignments,
-SUM(CASE
-    WHEN b.BookingStatus = 'Confirmed' THEN 1
-    ELSE 0 END) AS successful_bookings,
+FROM space_travel_agents ta
+
+
+
+LEFT JOIN
+
+(
+
+
+
+SELECT
+ah.agentid,
+ 
+COUNT(DISTINCT ah.assignmentid) AS total_assignments,
  
 SAFE_DIVIDE(
-            SUM(CASE
-    WHEN b.BookingStatus = 'Confirmed' THEN 1
-    ELSE 0 END), SUM(DISTINCT ah.AssignmentID))*100 AS booking_success_rate,
+COUNT(DISTINCT CASE WHEN b.bookingstatus = 'Confirmed' THEN ah.assignmentid END),COUNT(DISTINCT ah.assignmentid)) * 100 AS overall_success_rate,
+ 
+AVG(CASE WHEN b.bookingstatus = 'Confirmed' THEN b.totalrevenue END) AS overall_avg_revenue,
+ 
+ 
+ -- # of assignments with same launch and destination
+COUNT(DISTINCT CASE
+                WHEN b.destination = ci.input_destination AND b.launchlocation = ci.input_launchlocation
+                THEN b.assignmentid END) AS route_assignments,
+ 
+ -- # of CONFIRMED assignments with same launch and destination /  -- # of assignments with same launch and destination
+SAFE_DIVIDE(
+COUNT(DISTINCT CASE
+                WHEN b.destination = ci.input_destination AND b.launchlocation = ci.input_launchlocation AND b.bookingstatus = 'Confirmed' THEN b.assignmentid END),
+NULLIF(COUNT(DISTINCT CASE
+                WHEN b.destination = ci.input_destination AND b.launchlocation = ci.input_launchlocation THEN b.assignmentid END), 0)) * 100 AS route_success_rate,
+ 
+ 
+  -- # of assignments with same launch and destination - AVG REVENUE
+AVG(CASE
+    WHEN b.destination = ci.input_destination AND b.launchlocation = ci.input_launchlocation AND b.bookingstatus = 'Confirmed' THEN b.totalrevenue END) AS route_avg_revenue,
+ 
+ 
+ -- # of assignments with same leadsource and communication method
+COUNT(DISTINCT CASE
+                WHEN ah.communicationmethod = ci.input_communicationmethod AND ah.leadsource = ci.input_leadsource THEN b.assignmentid END) AS channel_assignments,
+ 
+ 
+  -- # of COMFIRMED assignments with same leadsource and communication method /  -- # of assignments with same leadsource and communication method
+SAFE_DIVIDE(
+COUNT(DISTINCT CASE
+                WHEN ah.communicationmethod = ci.input_communicationmethod AND ah.leadsource = ci.input_leadsource AND b.bookingstatus = 'Confirmed' THEN b.assignmentid END),
+NULLIF(COUNT(DISTINCT CASE
+                      WHEN ah.communicationmethod = ci.input_communicationmethod AND ah.leadsource = ci.input_leadsource THEN b.assignmentid END), 0)) * 100 AS channel_success_rate
+ 
+ 
 
-SUM(CASE
-    WHEN b.BookingStatus = 'Confirmed' THEN b.totalrevenue
-    ELSE 0 END) AS total_revenue
 
 
 
 
+FROM assignment_history ah
+CROSS JOIN 
+(
+select 
+'Niloo Torabifard' as input_customername,
+'Text' as input_communicationmethod,
+'Organic' as input_leadsource,
+'Venus' as input_destination,
+'New York Orbital Gateway' as input_launchlocation
+) ci--customer_input
 
 
-from assignment_history ah
 
-LEFT JOIN space_travel_agents ta
-ON ah.agentid = ta.agentid
-
-LEFT JOIN bookings b
-ON ah.assignmentid =b.assignmentid
-
-
-where ah.agentid = 21
-
-group by 1,2,3,4,5,6,7,8--,9--,10--,11,12,13--,14--,15
+LEFT JOIN bookings b ON ah.assignmentid = b.assignmentid
+GROUP BY ah.agentid
+) s--stats
+ON ta.agentid = s.agentid
