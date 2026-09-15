b3ab9f24971bf88e525225787791ab62a744df45
Niloofar Torabifard <33426204+niloofartf@users.noreply.github.com>
2026-09-15 16:18:30 -0400

    added performance metrics
    added performance metrics and also tested data for aggregated level for one random agent to verify

--- Astra_EI_AgentsRanking.sql
+++ Astra_EI_AgentsRanking.sql
@@ -1,9 +1,9 @@
 select 
 
 
-ah.customername,
-ah.communicationmethod,
-ah.leadsource,
+--ah.customername,
+--ah.communicationmethod,
+--ah.leadsource,
 ah.agentid,
 ta.firstname,
 ta.lastname,
@@ -12,13 +12,32 @@ ta.jobtitle,
 ta.spacelicensenumber,
 ta.yearsofservice,
 ta.averagecustomerservicerating,
-b.destination,
-b.launchlocation,
-b.bookingcompletedate,
-b.bookingstatus,
+--b.destination,
+--b.launchlocation,
+--b.bookingcompletedate,
+--b.bookingstatus,
+
+
+
 SUM(b.packagerevenue) as packagerevenue,
 SUM(b.destinationrevenue) as destinationrevenue,
-SUM(b.totalrevenue) as totalrevenue
+SUM(b.totalrevenue) as totalrevenue,
+
+
+
+SUM(DISTINCT ah.AssignmentID) AS total_assignments,
+SUM(CASE
+    WHEN b.BookingStatus = 'Confirmed' THEN 1
+    ELSE 0 END) AS successful_bookings,
+ 
+SAFE_DIVIDE(
+            SUM(CASE
+    WHEN b.BookingStatus = 'Confirmed' THEN 1
+    ELSE 0 END), SUM(DISTINCT ah.AssignmentID))*100 AS booking_success_rate,
+
+SUM(CASE
+    WHEN b.BookingStatus = 'Confirmed' THEN b.totalrevenue
+    ELSE 0 END) AS total_revenue
 
 
 
@@ -33,4 +52,7 @@ ON ah.agentid = ta.agentid
 LEFT JOIN bookings b
 ON ah.assignmentid =b.assignmentid
 
-group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+
+where ah.agentid = 21
+
+group by 1,2,3,4,5,6,7,8--,9--,10--,11,12,13--,14--,15
